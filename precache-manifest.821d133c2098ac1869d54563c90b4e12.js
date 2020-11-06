0c09356cd0a3d5d97797429565caa7d453eacb93
Valle <valletsf@gmail.com>
2020-11-06 19:38:31 +0100

    Updates

--- precache-manifest.821d133c2098ac1869d54563c90b4e12.js
+++ precache-manifest.821d133c2098ac1869d54563c90b4e12.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ec22ac504f68ac1bf98fee8b4d56ee2",
+    "revision": "1569465c24126c6d8d90341c21d776f5",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "4bef5f579b97cdaaae92",
-    "url": "/portfolio/static/css/2.e1d7071c.chunk.css"
+    "revision": "5f73fb66f4c361717b65",
+    "url": "/portfolio/static/css/2.b94aeec9.chunk.css"
   },
   {
-    "revision": "55ba3903931fa2d76f92",
+    "revision": "927ed520aa14291c6328",
     "url": "/portfolio/static/css/main.67ec99f9.chunk.css"
   },
   {
-    "revision": "4bef5f579b97cdaaae92",
-    "url": "/portfolio/static/js/2.a75906f5.chunk.js"
+    "revision": "5f73fb66f4c361717b65",
+    "url": "/portfolio/static/js/2.f842afc9.chunk.js"
   },
   {
-    "revision": "55ba3903931fa2d76f92",
-    "url": "/portfolio/static/js/main.628558e3.chunk.js"
+    "revision": "927ed520aa14291c6328",
+    "url": "/portfolio/static/js/main.316537a5.chunk.js"
   },
   {
     "revision": "53ebc025fca807d4dbea",
