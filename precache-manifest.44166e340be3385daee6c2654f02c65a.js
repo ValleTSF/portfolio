b5711ad5f3221c71e283d116af13ea797d9f1d49
Valle <valletsf@gmail.com>
2020-11-06 13:33:40 +0100

    Updates

---
+++ precache-manifest.44166e340be3385daee6c2654f02c65a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5deb7119d9bb6ffc4ac735d17237bbd9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4bef5f579b97cdaaae92",
+    "url": "/portfolio/static/css/2.e1d7071c.chunk.css"
+  },
+  {
+    "revision": "97e291fd086251e86c54",
+    "url": "/portfolio/static/css/main.67ec99f9.chunk.css"
+  },
+  {
+    "revision": "4bef5f579b97cdaaae92",
+    "url": "/portfolio/static/js/2.a75906f5.chunk.js"
+  },
+  {
+    "revision": "97e291fd086251e86c54",
+    "url": "/portfolio/static/js/main.826c449e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ecbf37fd7913d80fc2f9d493859ff47f.js"
+  "/portfolio/precache-manifest.44166e340be3385daee6c2654f02c65a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
