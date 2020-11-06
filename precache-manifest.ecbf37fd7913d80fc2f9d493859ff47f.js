05739d7a390b17efe8afb20e9da7b77c64600968
Valle <valletsf@gmail.com>
2020-11-06 11:45:09 +0100

    Updates

---
+++ precache-manifest.ecbf37fd7913d80fc2f9d493859ff47f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17eede36ee47fd1a1b3a7de718dd771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e080f8a753cb9af3729",
+    "url": "/static/css/2.894ef63d.chunk.css"
+  },
+  {
+    "revision": "21aa70eac2671a2b1f49",
+    "url": "/static/css/main.67ec99f9.chunk.css"
+  },
+  {
+    "revision": "9e080f8a753cb9af3729",
+    "url": "/static/js/2.a75906f5.chunk.js"
+  },
+  {
+    "revision": "21aa70eac2671a2b1f49",
+    "url": "/static/js/main.98e685a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
